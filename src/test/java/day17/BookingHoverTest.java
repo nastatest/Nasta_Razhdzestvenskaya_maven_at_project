b5f7ea22cb561809ffb6f27919bbf4cda5ad5f20
Razhdzestvenskaya <test.nr.ita@gmail.com>
2022-12-14 00:16:50 +0300

    added annotations

---
+++ src/test/java/day17/BookingHoverTest.java
@@ -0,0 +1,23 @@
+package day17;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BookingHoverTest {
+
+    @Before
+    public void precondition() {
+
+    }
+
+    @Test
+    public void testing() {
+
+    }
+
+    @After
+    public void closing() {
+
+    }
+}
---
+++ src/test/java/day17/BookingMoscowTest3.java
@@ -0,0 +1,23 @@
+package day17;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BookingMoscowTest3 {
+
+    @Before
+    public void precondition() {
+
+    }
+
+    @Test
+    public void testing() {
+
+    }
+
+    @After
+    public void closing() {
+
+    }
+}
